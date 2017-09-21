ddfcceebaf42c858afc59eb0da642734c3635c5c
DengHong <987809534@qq.com>
2017-09-21 10:32:14 +0800

    修改建表语句的bug

--- app/src/main/java/com/denghong/coolweather/database/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/denghong/coolweather/database/CoolWeatherOpenHelper.java
@@ -16,7 +16,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
     public static final String CREATE_PROVINCE = "create table Province (" +
             "id integer primary key autoincrement," +
             "province_name text," +
-            "province_code text";
+            "province_code text)";
 
     /**
      * 创建City表语句
@@ -25,7 +25,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
             "id integer primary key autoincrement," +
             "city_name text, " +
             "city_code text," +
-            "province_id integer";
+            "province_id integer)";
 
     /**
      * 创建County表语句
@@ -34,7 +34,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
             "id integer primary key autoincrement," +
             "county_name text," +
             "county_code text," +
-            "city_id integer";
+            "city_id integer)";
 
     public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
